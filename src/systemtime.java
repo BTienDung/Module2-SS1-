b4a22eb36f2f9b20bb5e0a18e528f9013b1debc8
BTienDung <tiendung0899@gmail.com>
2019-05-09 21:35:56 +0700

    nb

---
+++ src/systemtime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class systemtime {
+    public static void main(String[] args) {
+        Date time = new Date();
+        System.out.println("Time is: "+time);
+    }
+}
